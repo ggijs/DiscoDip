5218252c3d301a126bca8e3597648e81ff20fc9d
ggijs <gijs.gruppen@gmail.com>
2018-04-30 17:49:39 +0200

    chat channel added

---
+++ src/discord/data/chat_channel.py
@@ -0,0 +1,25 @@
+import discord.utility as u
+
+class chat_channel():
+    
+    def __init__(self, channel):
+        #Guaranteed
+        self.id = channel["id"]
+        self.type = channel["type"]
+
+        #Optional
+        self.guild_id = u.get_safe(channel, "guild_id")
+        self.position = u.get_safe(channel, "position")
+        self.permission_overwrites = u.get_safe(channel, "permission_overwrites")
+        self.name = u.get_safe(channel, "name")
+        self.topic = u.get_safe(channel, "topic")
+        self.nsfw = u.get_safe(channel, "nsfw")
+        self.last_message_id = u.get_safe(channel, "last_message_id")
+        self.bitrate = u.get_safe(channel, "bitrate")
+        self.user_limit = u.get_safe(channel, "user_limit")
+        self.recipients = u.get_safe(channel, "recipients")
+        self.icon = u.get_safe(channel, "icon")
+        self.owner_id = u.get_safe(channel, "owner_id")
+        self.application_id = u.get_safe(channel, "application_id")
+        self.parent_id = u.get_safe(channel, "parent_id")
+        self.last_pin_timestamp = u.get_safe(channel, "last_pin_timestamp")
