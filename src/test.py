9c85db9faad9d74ae2c1b9a62208bb3e11012116
ggijs <gijs.gruppen@gmail.com>
2018-04-30 20:47:42 +0200

    printer function on emoji class

--- src/test.py
+++ src/test.py
@@ -1,5 +1,25 @@
-from discord import *
-import logintoken as tokens
+#from discord import *
+#import logintoken as tokens
 
-connection = discord.Discord(tokens.get_token())
-connection.connect()
+#connection = discord.Discord(tokens.get_token())
+#connection.connect()
+
+import discord.data.emoji as emoji
+
+eex = {
+  "id": "41771983429993937",
+  "name": "LUL",
+  "roles": [ "41771983429993000", "41771983429993111" ],
+  "user": {
+    "username": "Luigi",
+    "discriminator": "0002",
+    "id": "96008815106887111",
+    "avatar": "5500909a3274e1812beb4e8de6631111"
+  },
+  "require_colons": 'true',
+  "managed": 'false',
+  "animated": 'false'
+}
+
+flut = emoji.Emoji(eex)
+print flut
